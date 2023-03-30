35af4100595e7823fd4baf95cfe6dfbfc708df66
Sean Davies <seandavies@unm.edu>
2023-03-30 11:13:49 -0600

    Added methods to interface

--- src/ManagerInterface.java
+++ src/ManagerInterface.java
@@ -1,2 +1,18 @@
+import java.util.LinkedList;
+
 public interface ManagerInterface {
+
+    void initialData(LinkedList data);
+
+    void initializeSilos();
+
+    LinkedList returnSiloData(Silo s);
+
+    void start();
+
+    void stop();
+
+    void pause();
+
+    void step();
 }
