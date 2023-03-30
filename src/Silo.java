6297091ef5bfeeedccd95d686c98c43fe7cdbee0
Sean Davies <seandavies@unm.edu>
2023-03-30 11:19:00 -0600

    Added methods to Silo

--- src/Silo.java
+++ src/Silo.java
@@ -1,2 +1,79 @@
+import java.util.LinkedList;
+
 public class Silo {
+
+    Silo(LinkedList data){
+
+    }
+
+    void runLine(){
+
+    }
+
+    Integer returnLine(){
+
+        return -1;
+    }
+
+    LinkedList returnData(){
+
+        return null;
+    }
+
+    void label(){
+
+    }
+
+    void noop(){
+
+    }
+
+    void move(){
+
+    }
+
+    void swap(){
+
+    }
+
+    void save(){
+
+    }
+
+    void add(){
+
+    }
+
+    void sub(){
+
+    }
+
+    void negate(){
+
+    }
+
+    void jump(){
+
+    }
+
+    void jez(){
+
+    }
+
+    void jnz(){
+
+    }
+
+    void jgz(){
+
+    }
+
+    void jlz(){
+
+    }
+
+    void jro(){
+
+    }
+
 }
