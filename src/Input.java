125f6ace40832d6d9146be6cf364578e5e430efd
Sean Davies <seandavies@unm.edu>
2023-04-18 18:00:12 -0600

    Input/Output Class

--- src/Input.java
+++ src/Input.java
@@ -1,2 +1,15 @@
+import java.util.LinkedList;
+
 public class Input {
+
+    private Integer row, column;
+    private LinkedList<String> initialInputs = new LinkedList();
+    private LinkedList<String> currentInputs = new LinkedList<>();
+
+    Input(LinkedList inputs, Integer row, Integer column){
+        this.row = row;
+        this.column = column;
+        this.initialInputs.addAll(inputs);
+
+    }
 }
--- src/Output.java
+++ src/Output.java
@@ -1,2 +1,11 @@
+import java.util.LinkedList;
+
 public class Output {
+    private Integer row, column;
+
+
+    Output(Integer row, Integer column){
+        this.row = row;
+        this.column = column;
+    }
 }
